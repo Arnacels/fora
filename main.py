c0e61e7d714cbaec56184af1d99d548cebb951fb
vysotskiyklightit <vysotskiy.k.lightit@gmail.com>
2021-10-31 01:38:12 +0300

    fix learn< added save

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from datetime import timedelta, datetime
 
 from fastai.collab import tabular_learner
-from fastai.data.transforms import Normalize
-from fastai.metrics import accuracy
-from fastai.tabular.core import Categorify, FillMissing
+from fastai.data.transforms import Normalize, RandomSplitter
+from fastai.metrics import accuracy, rmse
+from fastai.tabular.core import Categorify, FillMissing, TabularPandas
 from fastai.tabular.data import TabularDataLoaders
+from fastai.tabular.learner import TabularLearner
+from fastcore.basics import range_of
 from matplotlib import pyplot as plt
-from pandas import DataFrame
+from pandas import DataFrame, Series
+from sklearn.metrics import r2_score
 
-from db import products, train, test
+from db import products, train, test, geo
 from split_date import SplitDate
 from utils import cut_date
 from validate import ValidateColumns
@@ -18,12 +21,18 @@ TROPIC_ID = 5551018
 YOGURT_ID = 5550259
 
 GEO_CLUSTER = 2053
+CITY_ID = 1
 
 
-def get_sku_by_type(TYPE_ID):
-    water = products.filter_by_type(TYPE_ID)
+def get_sku_by_type(type_id):
+    water = products.filter_by_type(type_id)
     skus = water.SKU.to_list()
     return skus
+
+
+def get_geo_clusters_by_city(city_id):
+    frame = geo.pd.loc[geo.pd.cityId == city_id]
+    return frame.geoCluster.to_list()
 
 
 def plot_show(data_frame):
@@ -33,21 +42,27 @@ def plot_show(data_frame):
     plt.show()
 
 
-def get_by_sku_type_id(data_frame, sku_type_id):
+def filter_by_sku_type_id(data_frame, sku_type_id):
     skus = get_sku_by_type(sku_type_id)
-    water_in = data_frame.loc[data_frame['SKU'].isin(skus)]
-    return water_in
+    frame = data_frame.loc[data_frame['SKU'].isin(skus)]
+    return frame
 
 
-def get_by_cluster_id(data_frame, geo_cluster_id):
+def filter_by_cluster_id(data_frame, geo_cluster_id):
     data_frame = data_frame.loc[
         data_frame.geoCluster == geo_cluster_id].sort_values('date')
     return data_frame
 
 
+def filter_by_city_id(data_frame, city_id):
+    clusters = get_geo_clusters_by_city(city_id)
+    frame = data_frame.loc[data_frame['geoCluster'].isin(clusters)]
+    return frame
+
+
 def group_by_weeks():
-    water = get_by_sku_type_id(train.pd, WATER_ID)
-    water_in_geo = get_by_cluster_id(water, GEO_CLUSTER)
+    water = filter_by_sku_type_id(train.pd, WATER_ID)
+    water_in_geo = filter_by_cluster_id(water, GEO_CLUSTER)
 
     grouped_frame_1_week = cut_date(water_in_geo)
     grouped_frame_10_weeks = cut_date(
@@ -63,12 +78,35 @@ def clean_columns(data_frame):
     return data_frame
 
 
-def test_learn():
-    frame_skus_group_train = get_by_sku_type_id(train.pd, WATER_ID)
-    # frame_skus_group_train = get_by_cluster_id(frame_skus_group_train, GEO_CLUSTER)
+def set_mean_instead_of_null(data_frame: DataFrame):
+    groped_by_sku = data_frame.groupby(by=['SKU']).mean()
 
-    frame_skus_group_test = get_by_sku_type_id(test.pd, WATER_ID)
-    # frame_skus_group_test = get_by_cluster_id(frame_skus_group_test, GEO_CLUSTER)
+    series_map = {
+        'price': [],
+        'sales': []
+    }
+
+    for index, series in groped_by_sku.iterrows():
+        for column in series_map:
+            temp_df = data_frame.loc[data_frame['SKU'] == index]
+            mean_value = groped_by_sku[column][index]
+            fill_series = temp_df[column].fillna(mean_value)
+            series_map[column] = series_map[column] + fill_series.to_list()
+
+    for column_name, series in series_map.items():
+        data_frame = data_frame.drop(columns=[column_name])
+        data_frame[column_name] = series
+
+    return data_frame
+
+
+def test_learn():
+    # frame_skus_group_train = filter_by_sku_type_id(train.pd, WATER_ID)
+    frame_skus_group_train = filter_by_city_id(train.pd, CITY_ID)
+    frame_skus_group_train = set_mean_instead_of_null(frame_skus_group_train)
+
+    # frame_skus_group_test = filter_by_sku_type_id(test.pd, WATER_ID)
+    frame_skus_group_test = filter_by_city_id(test.pd, CITY_ID)
 
     learn_frame: DataFrame = frame_skus_group_train
     learn_frame = clean_columns(learn_frame)
@@ -82,34 +120,40 @@ def test_learn():
     test_frame = clean_columns(test_frame)
     test_frame = test_frame.loc[test_frame.price.notnull()]
     test_frame = SplitDate(test_frame).split()
-    test_frame['sales'] = 1
+    test_frame['sales'] = 0
 
-    dls = TabularDataLoaders.from_df(
-        learn_frame,
-        y_names='sales',
-        cat_names=[
-            'SKU',
-            'year',
-            'geoCluster',
-            'month',
-            'day',
-            'weekday'],
-        cont_names=[
-            'price',
-            'sales'],
-        procs=[Categorify, FillMissing, Normalize]
-    )
+    splits = RandomSplitter(valid_pct=0.2)(range_of(learn_frame))
+    to = TabularPandas(learn_frame,
+                       y_names='sales',
+                       cat_names=[
+                           'geoCluster',
+                           'SKU',
+                           'year',
+                           'month',
+                           'day',
+                           'weekday'],
+                       cont_names=[
+                           'price',
+                           'sales'],
+                       procs=[Categorify, FillMissing],
+                       splits=splits
+                       )
+    dls = to.dataloaders(2048)
+    dls.show_batch()
 
-    print(type(dls), dls)
-    # dls.show_batch()
-
-    learn = tabular_learner(dls, metrics=accuracy)
-    learn.lr_find()
-    learn.fit_one_cycle(10)
+    learn: TabularLearner = tabular_learner(
+        dls,
+        layers=[300, 200, 100, 50],
+        model_dir='models',
+        metrics=[rmse, r2_score])
+    learn.lr_find(start_lr=1e-05, end_lr=1e+05, num_it=100)
+    learn.fit_one_cycle(25)
+    learn.save('stage 1')
     # learn.show_results()
 
     dl = learn.dls.test_dl(test_frame)
-    print(learn.get_preds(dl=dl))
+    preds, targs = learn.get_preds(dl=dl)
+    print(preds, targs)
 
 
 if __name__ == '__main__':
